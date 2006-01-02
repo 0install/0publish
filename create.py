f1525a2276a7508906e81f11c914f484b2ab7079
Thomas Leonard <talex5@gmail.com>
2006-01-02 15:20:38 +0000

    Added more comments to the template.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/injector/0publish@626 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- create.py
+++ create.py
@@ -2,44 +2,52 @@ import os
 from xml.dom import minidom, XMLNS_NAMESPACE
 from zeroinstall.injector.namespaces import XMLNS_IFACE
 
+
+_template = """<?xml version="1.0" ?>
+<?xml-stylesheet type='text/xsl'
+     href='http://0install.net/2006/stylesheets/interface.xsl'?>
+
+<interface xmlns="http://zero-install.sourceforge.net/2004/injector/interface">
+  <name>%s</name>
+  <summary>cures all ills</summary>
+  <description>
+    A longer, multi-line description of the behaviour of the
+    program goes here. State clearly what the program is for
+    (clearly enough that people who don't want it will
+    realise too).
+
+    Use a blank line to separate paragraphs.
+  </description>
+
+  <!-- Optionally, uncomment this to specify an icon: -->
+  <!-- <icon href='http://site/icon.png' type='image/png'/> -->
+
+  <!-- Optionally, uncomment this to give the address of
+       the signed master interface: -->
+  <!-- <feed-for interface='http://site/interface'> -->
+
+  <!-- Set 'main' to the relative path of your default
+        executable within the implementation's directory.
+	E.g.: "myprog" or "bin/myprog" -->
+  <group main='myprog'>
+    <!-- List any libraries your program needs here -->
+    <!--
+    <requires interface="http://site/library">
+      <environment insert="python" name="PYTHONPATH"/>
+    </requires>
+    -->
+
+    <!-- List all implementations here.
+         For local interfaces, '.' is a relative path from this
+	 interface file to the directory containing the program.
+	 Usually, you can just leave it as '.'.
+    -->
+    <implementation id="." version="0.1" released='Snapshot'/>
+  </group>
+</interface>
+"""
+
 def create(f):
 	assert not os.path.exists(f)
 	name = os.path.basename(f)
-	
-	impl = minidom.getDOMImplementation()
-	doc = impl.createDocument(XMLNS_IFACE, 'interface', None)
-
-	def text(name, value):
-		e = doc.createElementNS(XMLNS_IFACE, name)
-		e.appendChild(doc.createTextNode(value))
-		return e
-
-	root = doc.documentElement
-	root.setAttributeNS(XMLNS_NAMESPACE, 'xmlns', XMLNS_IFACE)
-	root.setAttribute('uri', "http://SITE/interfaces/%s" % name)
-
-	root.appendChild(doc.createComment(
-		" Set the uri attribute above to a location from which\n"
-		"      this file can be downloaded. Try to choose a\n"
-		"      location that won't change! "))
-
-	root.appendChild(text("name", name))
-	root.appendChild(text("summary", 'a brief one-line summary'))
-	root.appendChild(text("description",
-		"A longer, multi-line description of the behaviour of the\n"
-		"program goes here. State clearly what the program is for\n"
-		"(clearly enough that people who don't want it will\n"
-		"realise too)."))
-	
-	group = doc.createElementNS(XMLNS_IFACE, 'group')
-	root.appendChild(group)
-
-	group.appendChild(doc.createComment(
-		' Add dependencies and implementations here. '))
-	
-	doc.writexml(file(f, 'w'), addindent = " ", newl = '\n')
-
-	print "Wrote '%s'." % f
-	print "Now edit it in a text editor, and set the uri, summary "
-	print "and description as indicated. When you're ready to add an "
-	print "implementation, run 0publish on the file again."
+	return _template % (name.split('.', 1)[0])
