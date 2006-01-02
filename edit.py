595e2b4de7165f1fe28ebf4ae70e04b0a59f3df2
Thomas Leonard <talex5@gmail.com>
2006-01-02 15:21:00 +0000

    If EDITOR isn't set, try various fallbacks.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/injector/0publish@627 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- edit.py
+++ edit.py
@@ -1,4 +1,15 @@
 import tempfile, os
+from logging import info, debug
+
+path_dirs = os.environ.get('PATH', '/bin:/usr/bin').split(':')
+
+def available_in_path(command):
+	info("Checking for %s in $PATH", command)
+	for x in path_dirs:
+		debug("Checking for %s/%s", x, command)
+		if os.path.isfile(os.path.join(x, command)):
+			return True
+	return False
 
 def edit(data):
 	fd, tmp = tempfile.mkstemp(prefix = '0publish-')
@@ -6,12 +17,18 @@ def edit(data):
 		stream = os.fdopen(fd, 'w')
 		stream.write(data)
 		stream.close()
-		editor = os.environ.get('EDITOR', 'vi')
+		editor = os.environ.get('EDITOR', None)
+		if editor is None:
+			info("$EDITOR not set. Trying fallbacks...")
+			for editor in ['sensible-editor', 'nano', 'vi']: # ,'ed'] ;-)
+				if available_in_path(editor):
+					break
+			else:
+				raise Exception("No editor found. Try setting $EDITOR first.")
+		info("Editing tmp file with '%s %s'..." % (editor, tmp))
 		if os.spawnlp(os.P_WAIT, editor, editor, tmp):
-			raise Exception('Editing with $EDITOR ("%s") failed')
+			raise Exception('Editing with $EDITOR ("%s") failed' % editor)
 		new_data = file(tmp).read()
 	finally:
 		os.unlink(tmp)
-	if new_data == data:
-		raise Exception('Data unchanged after edit. Aborting.')
 	return new_data
