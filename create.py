f870569cc55f1966d1ee9ce5827ad2d4eadfa51c
Thomas Leonard <talex5@gmail.com>
2006-01-02 20:16:00 +0000

    Added --local to allow creation of feeds from local interfaces. Ensure data ends with a new-line (otherwise signing goes wrong!).
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/injector/0publish@632 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- create.py
+++ create.py
@@ -1,7 +1,15 @@
 import os
 from xml.dom import minidom, XMLNS_NAMESPACE
 from zeroinstall.injector.namespaces import XMLNS_IFACE
+from zeroinstall.injector import model, reader
 
+_local_template = """<?xml version="1.0" ?>
+<?xml-stylesheet type='text/xsl'
+     href='http://0install.net/2006/stylesheets/interface.xsl'?>
+
+<interface xmlns="http://zero-install.sourceforge.net/2004/injector/interface">
+</interface>
+"""
 
 _template = """<?xml version="1.0" ?>
 <?xml-stylesheet type='text/xsl'
@@ -24,7 +32,7 @@ _template = """<?xml version="1.0" ?>
 
   <!-- Optionally, uncomment this to give the address of
        the signed master interface: -->
-  <!-- <feed-for interface='http://site/interface'> -->
+  <!-- <feed-for interface='http://site/interface'/> -->
 
   <!-- Set 'main' to the relative path of your default
         executable within the implementation's directory.
@@ -51,3 +59,27 @@ def create(f):
 	assert not os.path.exists(f)
 	name = os.path.basename(f)
 	return _template % (name.split('.', 1)[0])
+
+def create_from_local(local):
+	iface = model.Interface(os.path.abspath(local))
+	reader.update(iface, local, local = True)
+	doc = minidom.parseString(_local_template)
+	root = doc.documentElement
+	def element(uri, localName, data):
+		root.appendChild(doc.createTextNode('\n'))
+		element = doc.createElementNS(uri, localName)
+		element.appendChild(doc.createTextNode(data))
+		return element
+	root.appendChild(element(XMLNS_IFACE, 'name', iface.name))
+	root.appendChild(element(XMLNS_IFACE, 'summary', iface.summary))
+	root.appendChild(element(XMLNS_IFACE, 'description', iface.description))
+
+	if not iface.feed_for:
+		raise Exception("No <feed-for> in '%s'; can't use it as a local feed." % local)
+	if len(iface.feed_for) != 1:
+		raise Exception("Multiple <feed-for> elements. Not supported, sorry!")
+	uri = iface.feed_for.keys()[0]
+
+	root.setAttribute('uri', uri)
+
+	return doc.toxml()
