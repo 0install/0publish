0d5700620bf3f1265a3755e325bf8f742da81465
Thomas Leonard <talex5@gmail.com>
2006-01-28 15:06:01 +0000

    Replaced -r with --set-id, --set-released, --set-stability and --set-version for easier scripting.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/trunk/0publish@652 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- release.py
+++ release.py
@@ -4,59 +4,32 @@ import time, re
 
 date_format = '\d{4}-\d{2}-\d{2}'
 
-def make_release(data):
+def make_release(data, id, version, released, stability):
 	"""Normally there's only one implementation, but we can cope with several."""
+	if released and released != 'Snapshot' and not re.match(date_format, released):
+		raise Exception('Invalid date format. Use YYYY-MM-DD.')
+
 	doc = minidom.parseString(data)
-	unreleased = []
+	unreleased = None
 	all_impls = doc.documentElement.getElementsByTagNameNS(namespaces.XMLNS_IFACE, 'implementation')
 	for x in all_impls:
-		released = x.getAttribute('released')
-		if released is None or not re.match(date_format, released):
-			unreleased.append((None, x))
-	if not unreleased:
+		_released = x.getAttribute('released')
+		if released is None or not re.match(date_format, _released):
+			if unreleased:
+				raise Exception('Multiple unreleased implementations!')
+			unreleased = x
+	if unreleased is None:
 		if len(all_impls) == 0:
 			raise Exception('No implementations in interface file!')
 		if len(all_impls) > 1:
 			raise Exception("Multiple implementations, but all are released. Aborting.")
-		impl = all_impls[0]
-		unreleased.append((impl.getAttribute('released'), impl))
+		unreleased = all_impls[0]
 	
-	for released, x in unreleased:
-		id = x.getAttribute('id')
-		version = x.getAttribute('version')
-		stability = x.getAttribute('stability')
-		print "Implementation '%s' (version: %s  released: %s  stability: %s)" % \
-				(id, version, x.getAttribute('released'), stability)
-		new_version = raw_input('Version [%s]: ' % version)
-		if new_version:
-			version = new_version
-		if not released:
-			released = time.strftime('%Y-%m-%d')
-		while True:
-			new_released = raw_input('Released [%s]: ' % released)
-			if not new_released:
-				break
-			if re.match(date_format, new_released):
-				released = new_released
-				break
-			print "Invalid date format. Use YYYY-MM-DD."
-
-		while True:
-			new_stability = raw_input('Stability [testing]: ').lower()
-			if not new_stability:
-				new_stability = model.testing.name
-			for level in model.stability_levels:
-				if level.startswith(new_stability):
-					stability = level
-					break
-			else:
-				print "Invalid level '%s'" % new_stability
-				print "Choose one of", ', '.join(model.stability_levels.keys())
-				continue
-			break
-
-		x.setAttribute('version', version)
-		x.setAttribute('released', released)
-		x.setAttribute('stability', stability)
+	# In future, we may want to bulk change implementations...
+	for x in [unreleased]:
+		if id is not None: x.setAttribute('id', id)
+		if version is not None: x.setAttribute('version', version)
+		if released is not None: x.setAttribute('released', released)
+		if stability is not None: x.setAttribute('stability', stability)
 	
 	return doc.toxml()
