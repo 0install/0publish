bb4a9d12cf2d531fdded72119ed22ef320b666dc
Thomas Leonard <talex5@gmail.com>
2006-02-01 19:42:12 +0000

    Added --stable.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/trunk/0publish@660 9f8c893c-44ee-0310-b757-c8ca8341c71e

---
+++ stable.py
@@ -0,0 +1,27 @@
+from xml.dom import minidom
+from zeroinstall.injector import namespaces, model
+
+def mark_stable(data):
+	"""Find the single release marked as 'testing' and make it 'stable'."""
+	doc = minidom.parseString(data)
+	testing = []
+	all_impls = doc.documentElement.getElementsByTagNameNS(namespaces.XMLNS_IFACE, 'implementation')
+	for x in all_impls:
+		if get_stability(x) == 'testing':
+			testing.append(x)
+	if len(testing) == 0:
+		raise Exception('No implementations are currently "testing"!')
+	if len(testing) > 1:
+		raise Exception("Multiple 'testing' implementations!")
+	
+	testing[0].setAttribute('stability', 'stable')
+	
+	return doc.toxml()
+
+def get_stability(x):
+	root = x.ownerDocument.documentElement
+	while x is not root:
+		stab = x.getAttribute('stability')
+		if stab is not None: return stab
+		x = x.parentNode
+	return 'testing'
