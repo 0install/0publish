c707308e5b558b0a92d4d053bcbda63243ef909d
Thomas Leonard <talex5@gmail.com>
2006-09-24 11:27:35 +0000

    Warn about unknown elements and attributes in interface files that are in the Zero Install namespace. This should detect some typos.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/trunk/0publish@1053 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- validator.py
+++ validator.py
@@ -1,10 +1,57 @@
 import os
-from zeroinstall.injector import model
+from zeroinstall.injector import model, namespaces
 from zeroinstall.injector.reader import InvalidInterface, update
+from xml.dom import minidom, Node, XMLNS_NAMESPACE
 import tempfile
-from logging import warn
+from logging import warn, info, debug
 
-def check(data):
+known_elements = {
+	'interface' : ['uri', 'min-injector-version'],
+	'name' : [],
+	'summary' : [],
+	'description' : [],
+	'homepage' : [],
+	'icon' : ['type', 'href'],
+	'feed' : ['src', 'arch'],
+	'feed-for' : ['interface'],
+
+	'group' : ['version', 'released', 'main', 'stability', 'arch'],
+	'implementation' : ['id', 'version', 'released', 'main', 'stability', 'arch'],
+	'archive' : ['href', 'size', 'extract', 'type', 'start-offset'],
+	'recipe' : [],
+	'requires' : ['interface'],
+	'version' : ['not-before', 'before'],
+	'environment' : ['name', 'insert', 'default'],
+}
+
+def checkElement(elem):
+	if elem.namespaceURI != namespaces.XMLNS_IFACE:
+		info("Note: Skipping unknown (but namespaced) element <%s>", elem.localName)
+		return	# Namespaces elements are OK
+
+	if elem.localName not in known_elements:
+		warn("Unknown Zero Install element <%s>.\nNon Zero-Install elements should be namespaced.", elem.localName)
+		return
+	
+	known_attrs = known_elements[elem.localName]
+
+	for (uri, name), value in elem.attributes.itemsNS():
+		if uri == XMLNS_NAMESPACE:
+			continue	# Namespace declarations are fine
+
+		if uri:
+			info("Note: Skipping unknown (but namespaced) attribute '%s'", name)
+			continue
+
+		if name not in known_attrs:
+			warn("Unknown Zero Install attribute '%s' on <%s>.\nNon Zero-Install attributes should be namespaced.",
+					name, elem.localName)
+		
+	for child in elem.childNodes:
+		if child.nodeType == Node.ELEMENT_NODE:
+			checkElement(child)
+
+def check(data, warnings = True):
 	fd, tmp_name = tempfile.mkstemp(prefix = '0publish-validate-')
 	os.close(fd)
 	tmp_iface = model.Interface(tmp_name)
@@ -14,7 +61,6 @@ def check(data):
 		tmp_file.close()
 		try:
 			update(tmp_iface, tmp_name, local = True)
-			return True
 		except InvalidInterface, ex:
 			raise
 		except Exception, ex:
@@ -22,3 +68,7 @@ def check(data):
 			raise InvalidInterface(str(ex))
 	finally:
 		os.unlink(tmp_name)
+	
+	if warnings:
+		doc = minidom.parseString(data)
+		checkElement(doc.documentElement)
