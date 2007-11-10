1017047cc0104d1a5670ed95f3d6f6c6ad5fc939
Thomas Leonard <talex5@gmail.com>
2007-11-10 11:26:26 +0000

    --set-released="" now removes the 'released' attribute.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/trunk/0publish@2062 9f8c893c-44ee-0310-b757-c8ca8341c71e

--- release.py
+++ release.py
@@ -54,7 +54,11 @@ def make_release(data, id, version, released, stability, main, arch):
 	# In future, we may want to bulk change implementations...
 	for x in [unreleased]:
 		if id is not None: x.setAttribute('id', id)
-		if released is not None: x.setAttribute('released', released)
+		if released is not None:
+			if released:
+				x.setAttribute('released', released)
+			else:
+				x.removeAttribute('released')
 		if stability is not None: x.setAttribute('stability', stability)
 		if main is not None: x.setAttribute('main', main)
 		if arch is not None: x.setAttribute('arch', arch)
