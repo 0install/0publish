c7bec9ce4be7df71d1581b0ba0a13541951d2efe
Thomas Leonard <talex5@gmail.com>
2007-11-13 19:01:05 +0000

    Added some tests.
    git-svn-id: file:///home/talex/Backups/sf.net/Subversion/zero-install/trunk/0publish@2078 9f8c893c-44ee-0310-b757-c8ca8341c71e

---
+++ tests/testvalidator.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2.4
+import sys, logging
+from StringIO import StringIO
+from zeroinstall.injector.reader import InvalidInterface
+import unittest
+
+sys.path.insert(0, '..')
+
+import validator
+
+header = """<?xml version="1.0" ?>
+<?xml-stylesheet type='text/xsl' href='http://0install.net/2006/stylesheets/interface.xsl'?>
+<interface xmlns="http://zero-install.sourceforge.net/2004/injector/interface">
+  <name>test</name>
+  <summary>for testing</summary>
+  <description>
+    This is for testing.
+  </description>
+  <homepage>http://0install.net</homepage>
+  <feed-for interface='http://0install.net/2007/tests/just-testing.xml'/>"""
+footer = """
+</interface>
+"""
+
+root_logger = logging.getLogger()
+
+def check(xml, expectWarnings = ""):
+	my_log_stream = StringIO()
+	my_handler = logging.StreamHandler(my_log_stream)
+
+	root_logger.addHandler(my_handler)
+	old_stderr = sys.stderr
+	try:
+		sys.stderr = my_log_stream
+		validator.check(xml)
+		warnings = my_log_stream.getvalue()
+	finally:
+		sys.stderr = old_stderr
+		root_logger.removeHandler(my_handler)
+	if expectWarnings:
+		assert expectWarnings in warnings, "Expected warning '%s' not in '%s'" % (expectWarnings, warnings)
+	elif warnings:
+		raise Exception("Unexpected warnings '%s'" % warnings)
+
+class TestValidator(unittest.TestCase):
+	def testSimple(self):
+		check(header + footer)
+
+	def testWarnings(self):
+		check(header + "<foo/>" + footer, expectWarnings = "Unknown Zero Install element <foo>")
+		check(header + "<foo:bar xmlns:foo='http://foo'/>" + footer)
+		check(header + "<group foo='bar'/>" + footer, expectWarnings = "Non Zero-Install attributes should be namespaced")
+	
+	def testInvalid(self):
+		try:
+			check(header)
+		except InvalidInterface, ex:
+			assert "Invalid XML" in str(ex), ex
+
+		try:
+			check(header + "<implementation/>" + footer)
+		except InvalidInterface, ex:
+			assert "Missing 'id' attribute" in str(ex), ex
+
+suite = unittest.makeSuite(TestValidator)
+if __name__ == '__main__':
+	sys.argv.append('-v')
+	unittest.main()
